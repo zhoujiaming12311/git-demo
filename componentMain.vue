1e1a676df493edc2061d8447ea4422c190017dbe
zhoujm <zhoujm@yusys.com.cn>
2020-12-22 21:06:00 +0800

    第三次提交

---
+++ componentMain.vue
@@ -0,0 +1,24 @@
+<template>
+	<buttonChild></buttonChild>
+<div>
+	(main-vue){{name}}
+</div>
+</template>
+
+<script>
+
+import childVue from "./componentChild.vue";
+	export default{
+		name:'main-vue',
+		data:function(){
+			return {
+				name:'zhoujiaming'
+			}
+		},
+		components:{
+			'buttonChild':childVue
+		}
+	}
+</script>
+<style>
+</style>
