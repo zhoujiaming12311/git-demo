8f98ef5f82fcd930925096eb507e98f84c1d5959
zhoujm <zhoujm@yusys.com.cn>
2020-12-22 20:48:35 +0800

    第二次提交

---
+++ componentChild.vue
@@ -0,0 +1,17 @@
+<template>
+	<button @click='count++'>(child_component)please click it：{{count}}</button>
+</template>
+
+<script>
+
+	export default {
+		name:'child-vue',
+		data:function(){
+			return {
+				count:0
+			}
+		}
+	}
+</script>
+<style>
+</style>
